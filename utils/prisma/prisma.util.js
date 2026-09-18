8b417c8cdd3d1b4572e276449a2fbe8f221ecef4
BaharVahabi <bahar.vhbii@gmail.com>
2026-09-18 23:29:16 +0330

    setup Prisma

---
+++ utils/prisma/prisma.util.js
@@ -0,0 +1,14 @@
+import "dotenv/config"
+
+import { PrismaClient } from "../../prisma/generated/prisma/client"
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({
+  adapter,
+})
+
+export { prisma }
