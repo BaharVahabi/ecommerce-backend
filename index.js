f4175d5b392fefa1e712726331f536fe5313a0f6
BaharVahabi <bahar.vhbii@gmail.com>
2026-09-21 23:58:31 +0330

    add new routers to index.js file

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express"
 import { userRouter } from "./routes/users/index.js"
+import { categoryRouter } from "./routes/categories/index.js"
+import { authRouter } from "./routes/auth/index.js"
+import { productRouter } from "./routes/products/index.js"
 const app = express()
 const port =3000
 app.use(express.json())
 
+app.use("/files",express.static("uploads"))
+
 app.use("/users", userRouter)
+app.use("/auth", authRouter)
+app.use("/categories", categoryRouter)
+app.use("/products", productRouter)
 app.listen(port,()=>{console.log("app started")})
