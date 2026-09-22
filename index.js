069bcfea0febf41ed553c774a8bec141a2d46f54
BaharVahabi <bahar.vhbii@gmail.com>
2026-09-23 01:42:01 +0330

    feat : implement user profile routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { userRouter } from "./routes/users/index.js"
 import { categoryRouter } from "./routes/categories/index.js"
 import { authRouter } from "./routes/auth/index.js"
 import { productRouter } from "./routes/products/index.js"
+import { favoriteRouter } from "./routes/favorites/index.js"
 const app = express()
 const port =3000
 app.use(express.json())
@@ -13,4 +14,5 @@ app.use("/users", userRouter)
 app.use("/auth", authRouter)
 app.use("/categories", categoryRouter)
 app.use("/products", productRouter)
+app.use("/favorites", favoriteRouter)
 app.listen(port,()=>{console.log("app started")})
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,11 +1,78 @@
 import express from "express"
 import { prisma } from "../../utils/prisma/prisma.util.js"
+import { checkAuthentication } from "../../middleware/checkAuthentication.middleware.js"
+import { uploader } from "../../utils/files.util.js"
+import { customError } from "../../utils/errorHandler.js"
 
 const userRouter = express.Router()
 userRouter.get("/", async (request, response) => {
-    const data = await prisma.user.findMany()
+  const data = await prisma.user.findMany()
 
-    response.json(data)
+  response.json(data)
 })
 
+userRouter.get("/profiles",
+  checkAuthentication,
+  async (request, response) => {
+    const userId = request.user.id
+    const userImages = await prisma.userImage.findMany({
+      where: {
+        userId: userId
+      }
+    })
+    return response.json({
+      success: true,
+      data: userImages
+    })
+  }
+)
+userRouter.post(
+  "/profile",
+  checkAuthentication,
+  uploader.single("image"),
+  async (request, response) => {
+    const userId = request.user.id
+    const file = request.file
+
+    const userImage = await prisma.userImage.create({
+      data: {
+        image: file.filename,
+        userId: userId
+      }
+    })
+
+    return response.status(201).json({
+      success: true,
+      data: userImage,
+      message: "user image uploaded successfully"
+    })
+  }
+)
+userRouter.delete("/profiles/:id",
+  checkAuthentication,
+  async (request, response) => {
+    const userId = request.user.id
+    const imageId = request.params.id
+
+    const userImage = await prisma.userImage.findFirst({
+      where: {
+        id: imageId,
+        userId: userId,
+      }
+    })
+    if (!userImage) {
+      customError("user image not found ", 404)
+    }
+    await prisma.userImage.delete({
+      where: {
+        id: imageId
+      }
+    })
+    return response.json({
+      success: true,
+      message: "user image deleted successfully"
+    })
+  }
+
+)
 export { userRouter }
